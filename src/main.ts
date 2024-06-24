5d211507ceeaa902249b6c02e7b3817473d16618
dtamayo <franklin.tamayo@quito.gob.ec>
2024-06-24 09:29:07 -0500

    ft - swagger para la documentacion del API

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API SALUD MENTAL')
+    .setDescription('Salud mental')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
